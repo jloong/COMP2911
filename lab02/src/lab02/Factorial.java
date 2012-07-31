9ab1d89a8b545f2387080b58d489ffa5683ba484
Chi-Chun Tiao <ctia193@cse.unsw.EDU.AU>
2012-07-31 15:34:18 +1000

    added lab02 code

---
+++ lab02/src/lab02/Factorial.java
@@ -0,0 +1,43 @@
+package lab02;
+
+import java.math.BigInteger;
+
+public class Factorial
+{
+	int	n;
+	
+	public Factorial (int n)
+	{
+		// TODO
+	}
+	
+	public int asInt ()
+	{
+		// TODO
+		return 0;
+	}
+	
+	public long asLong ()
+	{
+		// TODO
+		return 0;
+	}
+	
+	public float asFloat ()
+	{
+		// TODO
+		return 0;
+	}
+	
+	public double asDouble ()
+	{
+		// TODO
+		return 0;
+	}
+	
+	public BigInteger asBigInteger ()
+	{
+		// TODO
+		return null;
+	}
+}
